78d6fde437ab81b5923658e6203ce8b7014288ee
WaqarAhmad321 <codewithwaqarahmad@gmail.com>
2025-08-27 03:47:56 +0500

    fix: change database url and move to production

--- apps/admin/src/lib/supabase.ts
+++ apps/admin/src/lib/supabase.ts
@@ -1,8 +1,7 @@
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = "https://rdwaplvdjhkpufdwdeht.supabase.co";
-const supabaseAnonKey =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJkd2FwbHZkamhrcHVmZHdkZWh0Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTI3MTE1NTIsImV4cCI6MjA2ODI4NzU1Mn0.TXeMgxOsLnhowByTU4kKyOKOb8c89kJQKHxEpz0sYZY";
+const supabaseUrl = process.env.SUPABASE_URL!;
+const supabaseAnonKey = process.env.SUPABASE_ANON_KEY!;
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
 
